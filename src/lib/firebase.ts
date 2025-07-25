57e75fd17e1f168ef3d30c5c05babdaed92cd8cc
meysam molie25 <meysammolie25@gmail.com>
2025-07-25 21:23:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // مقادیر نمونه برای جلوگیری از خطا در محیط توسعه
 // لطفاً این مقادیر را با پیکربندی واقعی پروژه Firebase خود جایگزین کنید.
 const firebaseConfig = {
-  apiKey: "AIzaSyA_...SAMPLE...", // نمونه کلید API
-  authDomain: "project.firebaseapp.com",
-  projectId: "project-id",
-  storageBucket: "project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
+  "projectId": "verdant-vault-7k4w3",
+  "appId": "1:983159235723:web:3e19f9d3265da955d1a08e",
+  "storageBucket": "verdant-vault-7k4w3.appspot.com",
+  "apiKey": "AIzaSyAy3Bv1uVw3t6av9JW2z60R4NOkgDV9nLg",
+  "authDomain": "verdant-vault-7k4w3.firebaseapp.com",
+  "messagingSenderId": "983159235723"
 };
 
 // Initialize Firebase
