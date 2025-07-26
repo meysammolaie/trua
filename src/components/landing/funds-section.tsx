1ac13e6bf15aecf58a5ef98104693bc0532ed9a1
meysam molie25 <meysammolie25@gmail.com>
2025-07-26 15:25:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/landing/funds-section.tsx
+++ src/components/landing/funds-section.tsx
@@ -1,3 +1,6 @@
+
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Bitcoin, Crown, Landmark, Medal } from "lucide-react";
 import { motion } from "framer-motion";
