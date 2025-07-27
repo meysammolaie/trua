020a7e2020e57265bcdaf3e521682e9e7c133ccf
meysam molie25 <meysammolie25@gmail.com>
2025-07-27 07:06:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/workspace/next.config.ts
@@ -0,0 +1,35 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Acknowledge that async_hooks is not available in the client-side bundle.
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
