ce5dad23abaa88a0a10ba622912ee6f3653f3114
meysammolaie <meysammolie@yahoo.com>
2025-07-31 22:26:58 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -23,6 +17,10 @@ const nextConfig: NextConfig = {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         async_hooks: false,
+        fs: false,
+        tls: false,
+        net: false,
+        http2: false,
       };
     }
     return config;
