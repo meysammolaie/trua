ad7a60c8ff090effeeb150c96af0e185fbc06959
meysammolaie <meysammolie@yahoo.com>
2025-08-01 02:04:02 +0330

    Create next.config.ts

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+        fs: false,
+        tls: false,
+        net: false,
+        http2: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
